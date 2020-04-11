03ba8262aa5baf4fd7fa15b33979cdeee24d6524
jardelfv <jardel.fv@gmail.com>
2020-04-11 13:31:36 -0300

    Criação do tutorial de instalação do LAMP

--- instalar-LAMP.sh
+++ instalar-LAMP.sh
@@ -4,3 +4,4 @@
 ##### Atenção! lembre-se de dar a permição de execução do arquivo: chmod +x arquivo.sh ####
 #### Script de instalação da pilha LAMP ###
 
+dialog --yesno 'sim ou não?' 0 0 ; echo Retorno: $?
