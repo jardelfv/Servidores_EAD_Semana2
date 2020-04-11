7cd4e92eb088ef158ae48be6ab18f4943558f206
Jardel Vasconcelos <jardel.fv@gmail.com>
2020-04-11 11:27:48 -0300

    commit inicial

---
+++ instalar-LAMP.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Jardel F.V. jardel.fv@gmail.com
+
+##### Atenção! lembre-se de dar a permição de execução do arquivo: chmod +x arquivo.sh ####
+#### Script de instalação da pilha LAMP ###
+
